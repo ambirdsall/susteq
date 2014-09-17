9442a54e0ae4ccbcb0f1f4d99f4045d5f0973b10
Melissa McCoy <apprentice@DBC-Student-Personal.local>
2014-09-17 16:26:02 -0700

    fixed the method for getting locations

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,6 +9,6 @@ class Admin::DashboardController < ApplicationController
     @total_water = dispensed_by_pump_for_all_table[:chartData]
     @shopkeepers_credit = credits_remaining_by_kiosk_table
     js :viz_data => [credits_by_kiosk_by_month, credits_bought_by_kiosk,credits_remaining_by_kiosk, dispensed_by_pump_by_month, errors_by_hub_chart, getHubs]
-    @new_hubs_ids = (Transaction.all - Hub.get_all_transactions).map { |transaction| transaction.location_id }.uniq
+    @new_hubs_ids = Transaction.all.map{|transaction| transaction.location_id}.uniq - Hub.all.map{|hub| hub.location_id}.uniq
   end
 end
