7a145ca1890c7f8860925ca34eecd8ed7426b583
Ori <opleban@gmail.com>
2014-09-19 11:12:53 -0700

    moves setTimeout div resize under chartData conditional, moves dashboar#main view javascript to intialize.js

--- app/assets/javascripts/big-data/initialize.js
+++ app/assets/javascripts/big-data/initialize.js
@@ -2,11 +2,21 @@ $(document).ready(function(){
   if (BigData.viz_data){
     var dataController = new BigData.DataController();
     dataController.getChartData(BigData.viz_data);
+
+    setTimeout(function(){
+      $('#page-wrapper').height($('html').height())
+    }, 100);
   }
+
   if ($('.dataTables-example').length){
     $('.dataTables-example').dataTable();
   }
-  setTimeout(function(){
-    $('#page-wrapper').height($('html').height())
-  }, 100);
+
+  if ($('.check-for-new-hubs').length){
+    $('#new-hub-alert').hide()
+    $('.check-for-new-hubs').on('click',function(){
+      console.log("inside listener")
+      $('#new-hub-alert').show();
+    })
+  }
 })
