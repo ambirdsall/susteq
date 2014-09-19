d1bf15998a99a7d59fa6e4f4d60510ebdcc3a672
Ori <opleban@gmail.com>
2014-09-19 05:30:58 -0700

    Improves/fixes TransactionController method * it now properly parses incoming JSON post request * adds basic controller tests to ensure that the route works and that a Transaction is created with a correctly formatted incoming request (i.e. JSON). * checks to see that new Transactions are correctly associated with existing hubs.

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -1,11 +1,20 @@
 class Admin::TransactionsController < ApplicationController
-  def create(transactions_json_array)
-    transactions_json_array.each do |transaction_data|
-      if hub = Hub.find_by(location_id:transaction_data.location_id)
-        hub.transactions.create(transaction_data)
-      else
-        Transaction.create(transaction_data)
+  def create
+    if params[:transactions]
+      params[:transactions].each do |transaction_data|
+        if hub = Hub.find_by(location_id:transaction_data[:location_id])
+          hub.transactions.create(transaction_params(transaction_data))
+        else
+          Transaction.create(transaction_params(transaction_data))
+        end
       end
     end
+    render text: "Thanks for sending a POST request. Payload: #{request.body.read}"
+  end
+
+  private
+
+  def transaction_params(transaction_data)
+    transaction_data.permit(:rfid_id, :starting_credits, :ending_credits, :transaction_code, :amount, :error_code, :longitude, :latitude, :location_id, :provider_id, :transaction_time)
   end
 end
