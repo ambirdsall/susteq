52ea3b6c18d639f54233aee93adf3362eb961cf0
Ori <opleban@gmail.com>
2014-09-19 00:45:11 -0700

    creates transactions create method which accepts array of JSON transactions, creates route

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -1,14 +1,12 @@
 class Admin::TransactionsController < ApplicationController
-  before_filter :require_admin_signin
 
-  def credits_by_kiosk
-    kiosk_total_objects = Transaction.select("location_id as kiosk_id, sum(amount) as total").credits_sold.group("location_id").order("sum(amount)")
-    obj_arr = kiosk_total_objects.map do |obj|
-      p obj
-      {location_id: obj.kiosk_id, total: obj.total}
-    end
-    respond_to do |format|
-      format.json { render json: obj_arr }
+  def create(array)
+    array.each do |transaction_data|
+      if hub = Hub.find_by(location_id:transaction_data.location_id)
+        hub.transactions.create(transaction_data)
+      else
+        Transaction.create(transaction_data)
+      end
     end
   end
 end
