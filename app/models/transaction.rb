a77e29d3329aba90b7560799e305a9dbea361e79
ambirdsall <ambirdsall@gmail.com>
2015-05-04 22:08:09 -0700

    Change transaction validation to new syntax.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
   belongs_to :transactable, polymorphic:true
-  validates_uniqueness_of :transaction_code, scope: [:location_id, :transaction_time]
+  validates :transaction_code, uniqueness: { scope: [:transaction_time, :transaction_code] }
   scope :credits_sold, -> { where(transaction_code: [20,21]) }
 
 end
