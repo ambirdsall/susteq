cc1c12533f26d321e7ce984f4c3ce49944438ec9
ambirdsall <ambirdsall@gmail.com>
2015-05-05 00:40:34 -0700

    Fix validation screw up.
    Scope transaction_code uniqueness by location_id, not by itself.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ActiveRecord::Base
   belongs_to :transactable, polymorphic:true
-  validates :transaction_code, uniqueness: { scope: [:transaction_time, :transaction_code] }
+  validates :transaction_code, uniqueness: { scope: [:transaction_time, :location_id] }
   scope :credits_sold, -> { where(transaction_code: [20,21]) }
 
 end
